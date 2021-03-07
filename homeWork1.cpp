fb16fc773e82d41f913e7234e7284806c4465464
wss8013 <wss8013@gmail.com>
2021-03-06 20:20:14 -0800

    add homework 1 to git

---
+++ homeWork1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*******************************************************
+Author: Shasha Wang
+This program is the first week's homework of CIS 22A-13Z.
+This program prints out some addition, multiplication,
+division and subtraction results.
+*******************************************************/
+
+int main()
+{
+    cout << "My name is Shasha Wang." << endl;
+    cout << "Today is September 24,2020."<<endl;
+    cout << "6565 + 1381 is " << 6565 + 1381 << endl;
+    cout << "6565 - 1241 is " <<6565 - 1241 << endl;
+    cout << "865 * 35 is " << 865 * 35 << endl;
+    cout << "8756 / 129 is " << 8756 / 129 << endl;
+    cout << "8756 / 129.0 is " << 8756 / 129.0 << endl;
+    //cout << 1 / 0 << endl; //Process returned -1073741676 (0xC0000094)
+    return 0;
+}
